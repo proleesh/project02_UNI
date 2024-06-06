c9fa4feea394e94c5fbc079daa734dcfcf04da90
sunghyuklee <merci726@yahoo.com>
2024-06-06 11:50:43 +0900

    update

--- src/main/java/org/proleesh/project02/controller/StudentController.java
+++ src/main/java/org/proleesh/project02/controller/StudentController.java
@@ -7,13 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/student")
-@RequiredArgsConstructor
+@Controller // 해당 Controller를 명시하여 Spring 컨테이너에게 맞긴다.
+@RequestMapping("/student") // 매핑할 경로
+@RequiredArgsConstructor // Lombok 에서 제공한 주입 애너테이션
 public class StudentController {
     // 4번의 장점?
     // DI(Dependency Injection)
@@ -21,13 +20,26 @@ public class StudentController {
     // 2. setter injection
     // 3. constructor injection
     // 4. Lombok Constructor Dependency Injection
+    /* 4번의 장점은 @RequireArgsConstructor 또는 @AllArgsConstructor 어노테이션을 사용하여,
+    자동으로 생성자를 생성해주기 때문에 코드가 간결해집니다.
+    - 코드가 줄어들어, 가독성이 좋아집니다.
+    - 의존성을 명확하게 하고, 불변성을 유지할 수 있습니다.
+    - 필드 주입과 같은 단점을 피하면서, 생성자 주입의 장점을 유지할 수 있습니다.
+    단점은 Lombok을 사용하지 않는 프로젝트에서는 사용할 수 없습니다.
+    Lombok 라이브러리에 대한 의존성이 생깁니다.
+     */
     private final StudentDAO studentDAO;
 
     // [1] 전채 학생 목록 조회
+    // HTTP Get 요청을 처리하는 핸들러 메서드로, "/lists" 경로로 들어오는 요청을 처리합니다.
     @GetMapping("/lists")
     public String listStudents(Model model){
+        // 모든 학생 정보를 가져오기 위해 studentDAO의 getAllStudents 메서드를 호출합니다.
         List<StudentDTO> students = studentDAO.getAllStudents();
+        // 가져온 학생 목록을 "students"라는 이름으로 모델에 추가합니다.
         model.addAttribute("students", students);
+        // "listStudents" 라는 뷰 이름을 반환합니다.
+        // 이 이름에 해당하는 뷰가 렌더링되어 클라이언트에게 응답으로 전송됩니다.
         return "listStudents";
     }
     // [2] 한 학생 목록 조회
--- src/main/java/org/proleesh/project02/dto/StudentDTO.java
+++ src/main/java/org/proleesh/project02/dto/StudentDTO.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@NoArgsConstructor // 파라미터(필드) 없는 즉 기본 생성자에 관한 애너테이션
+@AllArgsConstructor // 파라미터(필드) 있는 즉 매개변수 있는 생성자에 관한 애너테이션
+@Getter // Getter에 관한 애너테이션
+@Setter // Setter에 관한 애너테이션
 public class StudentDTO {
     private int id;
     private String name;
